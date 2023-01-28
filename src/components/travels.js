bdc3bf0bfb78db1f8e02098eaeb5ba3b00b6cf04
Jecy Jehu <jecyjehu@outlook.com>
2023-01-28 14:20:26 +0100

    new

--- src/components/travels.js
+++ src/components/travels.js
@@ -12,7 +12,7 @@ function Travels() {
         )
     })
     return (
-        <div className="mt-10 border border-gray-400 rounded-3xl space-y-4 pb-7">
+        <div className="mt-10 border border-gray-400 rounded-3xl space-y-4 pb-7">            
             <div className="flex items-center justify-center text-xl font-medium m-1 rounded-t-3xl bg-red-400 py-2 mb-8">
                 <img src={globe} alt="" />
                 <h1>My Travel Journal</h1>
