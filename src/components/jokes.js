a7fc909293d89b59997bc56c3f355e6dc64b56b4
Jecy Jehu <jecyjehu@outlook.com>
2023-02-16 13:48:56 +0100

    update

--- src/components/jokes.js
+++ src/components/jokes.js
@@ -1,9 +1,18 @@
+import {useState} from 'react'
+
 
 function Jokes(props) {
-    return (
+    const [isShown, setIsShown] = useState(false)
+    function toggleShown() {
+        console.log(isShown)
+        return setIsShown(prevIsShown => !prevIsShown)
+    }
+    return (        
         <div className="mt-5">
             {props.setup && <h3>Setup: {props.setup}</h3>}
-            <p>Punchline: {props.punchline}</p>
+            {isShown && <p>Punchline: {props.punchline}</p>}
+            {/* <p>Punchline: {props.punchline}</p> */}
+            <button className="border rounded-lg p-2 mb-2 bg-zinc-400" onClick={toggleShown}>Toggle</button>
             <hr />
         </div>
     )
